f33a3fdf02f25dafb5dcc99300237cf4b0cc5f0a
Pedro Flores <pedro_23_96@hotmail.com>
2021-06-30 22:14:31 +0200

    Funcion para descargar videos #4

---
+++ faceforensics/src/download.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+
+def download_youtube(link, name):
+    try: 
+        # object creation using YouTube
+        # which was imported in the beginning 
+        yt = YouTube(link) 
+    except: 
+        print("Connection Error") #to handle exception 
+    
+    # get the video with the extension and
+    # resolution passed in the get() function 
+    d_video = yt.streams.first()
+    try: 
+        # downloading the video 
+        d_video.download("/home/faceforensics/videos", filename=name) 
+    except: 
+        print("Ha habido algún error") 
+
+    print('Vídeo descargado') 
