85baa43afadd1cb8d736a7db48b3e325bbee2c64
Pedro Flores <pedro_23_96@hotmail.com>
2021-07-05 22:10:52 +0200

    Funcion descargar imagen close #8

--- reverse-eng/src/download.py
+++ reverse-eng/src/download.py
@@ -4,7 +4,7 @@ import os
 def downloadImage(url):
     r = requests.get(url)
 
-    if not os.path.exists('/home/reverse/data_test'):
+    if not os.path.exists('/home/reverse/data_test'): #Añadido por si se usa en local poder descargar varias
         os.mkdir('/home/reverse/data_test')
         os.mkdir('/home/reverse/data_test/0')
         
