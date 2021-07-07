e5633c3e99ebfd2eb60aec9ae0bdeefd3d72164a
Pedro Flores <pedro_23_96@hotmail.com>
2021-07-07 23:26:36 +0200

    Creador docker close #12

---
+++ kerasio/src/video_clasification.py
@@ -0,0 +1,209 @@
+"""
+Title: Video Classification with a CNN-RNN Architecture
+Author: [Sayak Paul](https://twitter.com/RisingSayak)
+Date created: 2021/05/28
+Last modified: 2021/06/05
+Description: Training a video classifier with transfer learning and a recurrent model on the UCF101 dataset.
+"""
+
+"""
+## Setup
+"""
+
+# from tensorflow_docs.vis import embed
+from tensorflow import keras
+from imutils import paths
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import imageio
+import cv2
+import os
+
+"""
+## Define hyperparameters
+"""
+
+IMG_SIZE = 224
+BATCH_SIZE = 64
+EPOCHS = 10
+
+MAX_SEQ_LENGTH = 20
+NUM_FEATURES = 2048
+
+# The following two methods are taken from this tutorial:
+# https://www.tensorflow.org/hub/tutorials/action_recognition_with_tf_hub
+
+
+def crop_center_square(frame):
+    y, x = frame.shape[0:2]
+    min_dim = min(y, x)
+    start_x = (x // 2) - (min_dim // 2)
+    start_y = (y // 2) - (min_dim // 2)
+    return frame[start_y : start_y + min_dim, start_x : start_x + min_dim]
+
+
+def load_video(path, max_frames=0, resize=(IMG_SIZE, IMG_SIZE)):
+    cap = cv2.VideoCapture(path)
+    frames = []
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = crop_center_square(frame)
+            frame = cv2.resize(frame, resize)
+            frame = frame[:, :, [2, 1, 0]]
+            frames.append(frame)
+
+            if len(frames) == max_frames:
+                break
+    finally:
+        cap.release()
+    return np.array(frames)
+
+
+"""
+We can use a pre-trained network to extract meaningful features from the extracted
+frames. The [`Keras Applications`](https://keras.io/api/applications/) module provides
+a number of state-of-the-art models pre-trained on the [ImageNet-1k dataset](http://image-net.org/).
+We will be using the [InceptionV3 model](https://arxiv.org/abs/1512.00567) for this purpose.
+"""
+
+
+def build_feature_extractor():
+    feature_extractor = keras.applications.InceptionV3(
+        weights="imagenet",
+        include_top=False,
+        pooling="avg",
+        input_shape=(IMG_SIZE, IMG_SIZE, 3),
+    )
+    preprocess_input = keras.applications.inception_v3.preprocess_input
+
+    inputs = keras.Input((IMG_SIZE, IMG_SIZE, 3))
+    preprocessed = preprocess_input(inputs)
+
+    outputs = feature_extractor(preprocessed)
+    return keras.Model(inputs, outputs, name="feature_extractor")
+
+
+feature_extractor = build_feature_extractor()
+
+"""
+The labels of the videos are strings. Neural networks do not understand string values,
+so they must be converted to some numerical form before they are fed to the model. Here
+we will use the [`StringLookup`](https://keras.io/api/layers/preprocessing_layers/categorical/string_lookup)
+layer encode the class labels as integers.
+"""
+
+label_processor = keras.layers.experimental.preprocessing.StringLookup(
+   num_oov_indices=0, mask_token='fake', vocabulary=np.unique(['real'])
+)
+
+# print(label_processor.get_vocabulary())
+
+
+# Utility for our sequence model.
+def get_sequence_model():
+    class_vocab = label_processor.get_vocabulary()
+
+    frame_features_input = keras.Input((MAX_SEQ_LENGTH, NUM_FEATURES))
+    mask_input = keras.Input((MAX_SEQ_LENGTH,), dtype="bool")
+
+    # Refer to the following tutorial to understand the significance of using `mask`:
+    # https://keras.io/api/layers/recurrent_layers/gru/
+    x = keras.layers.GRU(16, return_sequences=True)(
+        frame_features_input, mask=mask_input
+    )
+    x = keras.layers.GRU(8)(x)
+    x = keras.layers.Dropout(0.4)(x)
+    x = keras.layers.Dense(8, activation="relu")(x)
+    output = keras.layers.Dense(len(class_vocab), activation="softmax")(x)
+
+    rnn_model = keras.Model([frame_features_input, mask_input], output)
+
+    rnn_model.compile(
+        loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+    )
+    return rnn_model
+
+
+sequence_model = get_sequence_model()
+
+"""
+**Note**: To keep the runtime of this example relatively short, we just used a few
+training examples. This number of training examples is low with respect to the sequence
+model being used that has 99,909 trainable parameters. You are encouraged to sample more
+data from the UCF101 dataset using [the notebook](https://colab.research.google.com/github/sayakpaul/Action-Recognition-in-TensorFlow/blob/main/Data_Preparation_UCF101.ipynb) mentioned above and train the same model.
+"""
+
+"""
+## Inference
+"""
+
+
+def prepare_single_video(frames):
+    frames = frames[None, ...]
+    frame_mask = np.zeros(shape=(1, MAX_SEQ_LENGTH,), dtype="bool")
+    frame_featutes = np.zeros(shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32")
+
+    for i, batch in enumerate(frames):
+        video_length = batch.shape[1]
+        length = min(MAX_SEQ_LENGTH, video_length)
+        for j in range(length):
+            frame_featutes[i, j, :] = feature_extractor.predict(batch[None, j, :])
+        frame_mask[i, :length] = 1  # 1 = not masked, 0 = masked
+
+    return frame_featutes, frame_mask
+
+
+def sequence_prediction(path):
+    class_vocab = label_processor.get_vocabulary()
+
+    frames = load_video(path)
+    frame_features, frame_mask = prepare_single_video(frames)
+    probabilities = sequence_model.predict([frame_features, frame_mask])[0]
+
+    result = ""
+    resultProb = -1
+    for i in np.argsort(probabilities)[::-1]:
+        print(f"  {class_vocab[i]}: {probabilities[i] * 100:5.2f}%")
+        if probabilities[i] > resultProb:
+            result = class_vocab[i]
+            resultProb = probabilities[i]
+
+    print([{'0': result}])
+    return [{'0': result}]
+    # return frames
+
+
+def detect(video):
+    return sequence_prediction(video)
+
+
+"""
+## Next steps
+
+* In this example, we made use of transfer learning for extracting meaningful features
+from video frames. You could also fine-tune the pre-trained network to notice how that
+affects the end results.
+* For speed-accuracy trade-offs, you can try out other models present inside
+`tf.keras.applications`.
+* Try different combinations of `MAX_SEQ_LENGTH` to observe how that affects the
+performance.
+* Train on a higher number of classes and see if you are able to get good performance.
+* Following [this tutorial](https://www.tensorflow.org/hub/tutorials/action_recognition_with_tf_hub), try a
+[pre-trained action recognition model](https://arxiv.org/abs/1705.07750) from DeepMind.
+* Rolling-averaging can be useful technique for video classification and it can be
+combined with a standard image classification model to infer on videos.
+[This tutorial](https://www.pyimagesearch.com/2019/07/15/video-classification-with-keras-and-deep-learning/)
+will help understand how to use rolling-averaging with an image classifier.
+* When there are variations in between the frames of a video not all the frames might be
+equally important to decide its category. In those situations, putting a
+[self-attention layer](https://www.tensorflow.org/api_docs/python/tf/keras/layers/Attention) in the
+sequence model will likely yield better results.
+* Following [this book chapter](https://livebook.manning.com/book/deep-learning-with-python-second-edition/chapter-11),
+you can implement Transformers-based models for processing videos.
+"""
