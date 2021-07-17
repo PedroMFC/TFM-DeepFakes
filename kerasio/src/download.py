53fc109aaaa7ad86890d892d955c49c5555940c0
Pedro Flores <pedro_23_96@hotmail.com>
2021-07-17 13:20:27 +0200

    Arreglado try-except close #27

--- kerasio/src/download.py
+++ kerasio/src/download.py
@@ -4,14 +4,10 @@ def download_youtube(link, name):
     try: 
         # Creción del objeto usando Youtube
         yt = YouTube(link) 
-    except: 
-        print("Connection Error") # Manejar la excepción
-    
-    # Obtener el vídeo con la extensión y resolución
-    d_video = yt.streams.first()
-    try: 
-        # Descargar el vídeo
+        d_video = yt.streams.first()
+
         d_video.download("/home/kerasio/videos", filename=name) 
+
     except: 
         print("Ha habido algún error")
         return -1 
