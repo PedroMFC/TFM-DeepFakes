1376d39d93dfd6d464e7f7318f66e7dfe3476e66
Pedro Flores <pedro_23_96@hotmail.com>
2021-07-22 21:14:51 +0200

    Credenciales cache close #25

--- cache/src/firestore.py
+++ cache/src/firestore.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("/app/credentials/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
