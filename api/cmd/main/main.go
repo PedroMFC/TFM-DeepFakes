b1096905984a4e863d92827203929bd32bcef6a5
Pedro Flores <pedro_23_96@hotmail.com>
2021-08-14 18:59:59 +0200

    PRD api #38

--- api/cmd/main/main.go
+++ api/cmd/main/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"api/cmd/routes"
 	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	parar := make(chan int)
 
 	client := &http.Client{}
+	gin.SetMode(gin.ReleaseMode)
 	go routes.NewAppGin(client).Start()
 
 	<-parar
