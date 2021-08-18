a0f6933fc712fb7d448cffb1198da8cc3386b743
Pedro Flores <pedro_23_96@hotmail.com>
2021-08-18 15:44:59 +0200

    Mejoradas explicaciones #14

--- react-ui/src/components/pages/HomePage/Data.js
+++ react-ui/src/components/pages/HomePage/Data.js
@@ -5,8 +5,8 @@ export const homeObjOne = {
     topLine: '¿Qué son los DeepFakes?',
     headline: 'Actualmente suponen un reto en la lucha contra la desinformación',
     description:
-      'Deepfake generalmente se refiere a vídeos en los que la cara y / o la voz de una persona, generalmente una figura pública, ha sido manipulada utilizando software de inteligencia artificial de una manera que hace que el vídeo alterado se vea auténtico.\n' +
-      'Se consideran una fuente de preocupación porque a menudo se las utiliza para inducir a error intencionalmente, como hacer que parezca que un político dijo algo que no dijo, o hacer que parezca que una celebridad estaba en un vídeo pornográfico en el que no estaba.',
+      'El término deepfake generalmente se refiere a vídeos en los que la cara y / o la voz de una persona, generalmente una figura pública, ha sido manipulada utilizando software de inteligencia artificial de una manera que hace que el vídeo alterado se vea auténtico.\n' +
+      'Se consideran una fuente de preocupación porque, a menudo, se utilizan para inducir a error intencionalmente, como hacer que parezca que un político dijo algo que no dijo, o hacer que parezca que una celebridad estaba en un vídeo pornográfico en el que no estaba.',
     buttonLabel: 'Get Started',
     imgStart: '',
     img: 'img/isfake.svg',
@@ -20,8 +20,8 @@ export const homeObjOne = {
     topLine: 'FaceForensics++',
     headline: 'Herramienta para el análisis de vídeo',
     description:
-      'Utiliza este modelo entrenado sobre el conjunto de entrenamiento de Faceforensics++. Consiste en 1000 vídeos que han sido manipulados mediante cuatro métodos '+
-      'diferentes: Face2Face, FaceSwap, Deepfakes y NeuralTextures',
+      'Utiliza la red XceptionNet entrenada sobre el conjunto de entrenamiento de Faceforensics++. Consiste en 1000 vídeos que han sido manipulados mediante cuatro métodos '+
+      'diferentes: Face2Face, FaceSwap, Deepfakes y NeuralTextures.',
     buttonLabel: 'Learn More',
     imgStart: '',
     img: 'img/video.svg',
@@ -35,8 +35,8 @@ export const homeObjOne = {
     topLine: 'Reverse Engineering',
     headline: 'Herramienta para análisis de imágenes',
     description:
-      'Utiliza este algoritmo para la detecciónd de DeepFakes en imágenes desarrollado por Facebook. Este algorimto utiliza ingeniería inversa para saber si una imagen ' +
-      'ha sido manipulada, aprendiendo las "huellas" que dejan cada uno de los algoritmos',
+      'Utiliza este algoritmo para la detecciónd de DeepFakes en imágenes desarrollado por Facebook. Este algorimto, para saber si una imagen ' +
+      'ha sido manipulada, usa las "huellas" que dejan cada uno de los algoritmos de creación dintética de rostros.',
     buttonLabel: 'Start Now',
     imgStart: 'start',
     img: 'img/image.svg',
@@ -51,7 +51,7 @@ export const homeObjOne = {
     headline: 'Puedes incluir cualquier modelo',
     description:
       'Puedas ampliar fácilmente el número de algorimtos disponibles en esta arquitectura. Por ejemplo, podemos usar un clasificador de imágenes implementado en Keras ' + 
-      'para detectar si verdaderamente estás siendo engañado o no',
+      'para detectar si verdaderamente estás siendo engañado o no.',
     buttonLabel: 'Sign Up Now',
     imgStart: 'start',
     img: 'img/add.svg',
