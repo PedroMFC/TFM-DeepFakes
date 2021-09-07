38cb3cac5b6e16d1ad3064d84f47073e441fca2c
Pedro Flores <pedro_23_96@hotmail.com>
2021-09-07 16:12:50 +0200

    URLs en Keras img #41

--- kerasioimg/src/connect_cache.py
+++ kerasioimg/src/connect_cache.py
@@ -2,10 +2,12 @@
 from hash import getHash
 import requests
 
+URL_CH = 'https://cache-utoehvsqvq-ew.a.run.app'
+
 def get(path, service):
     hash_value = getHash(path)
 
-    url = 'https://cache-utoehvsqvq-ew.a.run.app/results/' + hash_value + '/' + service
+    url = URL_CH + '/results/' + hash_value + '/' + service
 
     r = requests.get(url)
 
@@ -14,16 +16,18 @@ def get(path, service):
     # print("En cache " + data['result'])
     # print("Resultado cache: " + data.result)
 
-    return data['result']
+    return data['result'], data['perFake'], data['perReal']
 
-def send(path, result, service):
+def send(path, result, service, perFake, perReal):
     hash_value = getHash(path)
 
-    url = 'https://cache-utoehvsqvq-ew.a.run.app/results'
+    url = URL_CH + '/results'
 
     data = {'sha': hash_value,
         'result':result,
-        'service':service
+        'service':service,
+        'perFake': perFake,
+        'perReal': perReal
         }
 
     requests.put(url, json = data)
