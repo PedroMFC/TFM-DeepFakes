fc14f70f6ef3ec94804c2c9bec7370ee85509b46
Pedro Flores <pedro_23_96@hotmail.com>
2021-09-24 11:12:40 +0200

    #36 FF++ mayor calidad

--- faceforensics/src/download.py
+++ faceforensics/src/download.py
@@ -4,9 +4,10 @@ def download_youtube(link, name):
     try: 
         # Creción del objeto usando Youtube
         yt = YouTube(link) 
-        d_video = yt.streams.first()
+        d_video = yt.streams.filter(adaptive=True).first()
 
         d_video.download("/home/faceforensics/videos", filename=name + ".mp4") 
+        #d_video.download("/app/videos", filename=name + ".mp4") 
 
     except: 
         print('Ha habido algún error al descargar el vídeo')
